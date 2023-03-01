24d6c8d81f4fe84ed3eddf7194b6e9406bef87f3
ShunPeng1 <shunpeng1@gmail.com>
2023-03-01 15:01:18 +0700

    refactor adding empty manager scripts

--- Assets/_Scripts/Calendar/CalendarCreationPage.cs
+++ Assets/_Scripts/Calendar/CalendarCreationPage.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using TMPro;
@@ -7,96 +6,99 @@ using UnityEngine;
 using Match = System.Text.RegularExpressions.Match;
 
 
-public class CalendarCreationPage : MonoBehaviour
+namespace _Scripts.Calendar
 {
-    [SerializeField] private TMP_InputField inputField;
-    [SerializeField] private TextMeshProUGUI comment;
-    [SerializeField] private TMP_Dropdown semesterDropdown;
-
-    private MatchCollection _semesterMatchCollection;
-    
-    void Start()
+    public class CalendarCreationPage : MonoBehaviour
     {
-        //Adds a listener to the main input field and invokes a method when the value changes.
-        //inputField.onValueChanged.AddListener(delegate
-        //{
+        [SerializeField] private TMP_InputField inputField;
+        [SerializeField] private TextMeshProUGUI comment;
+        [SerializeField] private TMP_Dropdown semesterDropdown;
+
+        private MatchCollection _semesterMatchCollection;
+    
+        void Start()
+        {
+            //Adds a listener to the main input field and invokes a method when the value changes.
+            //inputField.onValueChanged.AddListener(delegate
+            //{
             //InputCheck();
-        //    Test();
-        //});
+            //    Test();
+            //});
        
-    }
+        }
 
-    public void OnFastPaste()
-    {
-        string clipBoard = GUIUtility.systemCopyBuffer;
-        inputField.text = clipBoard;
-        OnInputCheck();
-    }
+        public void OnFastPaste()
+        {
+            string clipBoard = GUIUtility.systemCopyBuffer;
+            inputField.text = clipBoard;
+            OnInputCheck();
+        }
 
-    public void OnClearAll()
-    {
-        inputField.text = String.Empty;
-        OnInputCheck();
-    }
+        public void OnClearAll()
+        {
+            inputField.text = String.Empty;
+            OnInputCheck();
+        }
     
-    public void OnInputCheck()
-    {
-        string text = inputField.text;
-        string pattern =  @"Học kỳ (?<semester>\d) Năm học (?<yearFrom>\d+) - (?<yearTo>\d+)(\n|\r|\r|\n)[^(\n|\r|\r|\n)]*(\n|\r|\r|\n)[^(\n|\r|\r|\n)]*(\n|\r|\r|\n)(?<entries>(?:[^.](?!Tổng số tín chỉ đăng ký))*)";
-        
-        // Instantiate the regular expression object.
-        Regex r = new Regex(pattern);
-        
-        // Match the regular expression pattern against a text string.
-        _semesterMatchCollection = r.Matches(text);
-
-        if (_semesterMatchCollection.Count > 0)
+        public void OnInputCheck()
         {
-            SetDropdownInfo();
+            string text = inputField.text;
+            string pattern =  @"Học kỳ (?<semester>\d) Năm học (?<yearFrom>\d+) - (?<yearTo>\d+)(\n|\r|\r|\n)[^(\n|\r|\r|\n)]*(\n|\r|\r|\n)[^(\n|\r|\r|\n)]*(\n|\r|\r|\n)(?<entries>(?:[^.](?!Tổng số tín chỉ đăng ký))*)";
+        
+            // Instantiate the regular expression object.
+            Regex r = new Regex(pattern);
+        
+            // Match the regular expression pattern against a text string.
+            _semesterMatchCollection = r.Matches(text);
+
+            if (_semesterMatchCollection.Count > 0)
+            {
+                SetDropdownInfo();
+            }
+            else
+            {
+                Debug.Log("WHY NOT DETECT");
+            }
         }
-        else
+
+        public void OnSubmission()
         {
-            Debug.Log("WHY NOT DETECT");
+            int selectedSemester = semesterDropdown.value;
+            GroupCollection groups = _semesterMatchCollection[selectedSemester].Groups;
+            TimeTable timeTable = new TimeTable(Int32.Parse(groups["semester"].Value), Int32.Parse(groups["semester"].Value), groups["entries"].Value );
         }
-    }
 
-    public void OnSubmission()
-    {
-         int selectedSemester = semesterDropdown.value;
-         GroupCollection groups = _semesterMatchCollection[selectedSemester].Groups;
-         TimeTable timeTable = new TimeTable(Int32.Parse(groups["semester"].Value), Int32.Parse(groups["semester"].Value), groups["entries"].Value );
-    }
-
-    private void SetDropdownInfo()
-    {
-        List<TMP_Dropdown.OptionData> semesterDropdownOptions = new List<TMP_Dropdown.OptionData>();
-        
-        foreach (Match match in _semesterMatchCollection)
+        private void SetDropdownInfo()
         {
-            Debug.Log("Each Match is semester"+ match.Value);
+            List<TMP_Dropdown.OptionData> semesterDropdownOptions = new List<TMP_Dropdown.OptionData>();
+        
+            foreach (Match match in _semesterMatchCollection)
+            {
+                Debug.Log("Each Match is semester"+ match.Value);
             
-            TMP_Dropdown.OptionData semesterData = new TMP_Dropdown.OptionData();
-            string yearFrom = match.Groups["yearFrom"].Value.Substring( match.Groups["yearFrom"].Value.Length-2);
-            string semester = match.Groups["semester"].Value;
-            semesterData.text = "Học Kỳ " + yearFrom + semester;
-            semesterDropdownOptions.Add(semesterData);
+                TMP_Dropdown.OptionData semesterData = new TMP_Dropdown.OptionData();
+                string yearFrom = match.Groups["yearFrom"].Value.Substring( match.Groups["yearFrom"].Value.Length-2);
+                string semester = match.Groups["semester"].Value;
+                semesterData.text = "Học Kỳ " + yearFrom + semester;
+                semesterDropdownOptions.Add(semesterData);
 
-            semesterDropdown.options = semesterDropdownOptions;
+                semesterDropdown.options = semesterDropdownOptions;
+            }
         }
-    }
 
-    private void SetCommentWarning()
-    {
-        comment.gameObject.SetActive(true);
-    }
+        private void SetCommentWarning()
+        {
+            comment.gameObject.SetActive(true);
+        }
 
-    private void SetActiveCommentFalse()
-    {
-        comment.gameObject.SetActive(false);
+        private void SetActiveCommentFalse()
+        {
+            comment.gameObject.SetActive(false);
+        }
+    
+    
+    
+    
     }
-    
-    
-    
-    
 }
 

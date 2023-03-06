cfced5ab660235959a2573fc8b789f14ed6a1210
ShunPeng1 <shunpeng1@gmail.com>
2023-03-06 21:24:09 +0700

    refactor: Timetable TextMeshPro Infomation syntax

--- Assets/_Scripts/Calendar/TimeBlock.cs
+++ Assets/_Scripts/Calendar/TimeBlock.cs
@@ -7,11 +7,12 @@ public class TimeBlock : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI firstHeader;
     [SerializeField] private TextMeshProUGUI secondHeader;
-    
+
     public void Init(SubjectInfo subjectInfo)
     {
         firstHeader.text = subjectInfo.name;
-        secondHeader.text = subjectInfo.lessonStartHour.ToString() + " - " + subjectInfo.lessonEndHour.ToString() +
-                            " at " + subjectInfo.room;
+        secondHeader.text = subjectInfo.lessonStartHour.Hours + ":" + subjectInfo.lessonStartHour.Minutes.ToString("D2") + " - " +
+                            subjectInfo.lessonEndHour.Hours + ":" + subjectInfo.lessonEndHour.Minutes.ToString("D2") +
+                            " tại " + subjectInfo.room;
     }
 }

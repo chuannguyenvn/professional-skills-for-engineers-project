bfcef7d2488336ab817dca0321caf9afba99ba72
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-03-15 11:26:44 +0700

    feat: unfinished 3D buildings generation

--- Assets/_Scripts/Map/Building.cs
+++ Assets/_Scripts/Map/Building.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Shapes;
 using UnityEngine;
 
@@ -7,15 +8,46 @@ namespace Map
 {
     public class Building : MonoBehaviour
     {
+        public bool Is3D = true;
+        public float BuildingHeight = 10f;
+
         [SerializeField] private Polygon polygon;
-        
+        private List<Vector2> geoCoordinates;
+        private List<Vector2> worldCoordinates;
+
         public void Init(string buildingName, List<Vector2> coordinates)
         {
             gameObject.name = buildingName;
-            foreach (var coordinate in coordinates)
+            geoCoordinates = new(coordinates);
+            worldCoordinates = coordinates.Select(MapHelper.GeoToWorldPosition).ToList();
+            
+            if (Is3D) Init3D();
+            else Init2D();
+        }
+
+        private void Init2D()
+        {
+            foreach (var coordinate in worldCoordinates)
             {
-                var worldPos = MapHelper.GeoToWorldPosition(coordinate);
-                polygon.AddPoint(worldPos);
+                polygon.AddPoint(coordinate);
+            }
+        }
+
+        private void Init3D()
+        {
+            for (int i = 0; i < worldCoordinates.Count; i++)
+            {
+                var firstIndex = i;
+                var secondIndex = (i + 1) % worldCoordinates.Count;
+
+                var bottomFirstPoint = worldCoordinates[firstIndex];
+                var bottomSecondPoint = worldCoordinates[secondIndex];
+                var topSecondPoint = bottomSecondPoint + Vector2.up * BuildingHeight;
+                var topFirstPoint = bottomFirstPoint + Vector2.up * BuildingHeight;
+
+                var sidePolygon = Instantiate(polygon);
+                sidePolygon.transform.Rotate(Vector3.right, -90);
+                sidePolygon.AddPoints(new[] {bottomFirstPoint, bottomSecondPoint, topSecondPoint, topFirstPoint});
             }
         }
     }

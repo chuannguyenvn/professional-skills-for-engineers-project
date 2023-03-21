db9f732f99829f56f6d375f5bccaa3b254816ab8
ShunPeng1 <shunpeng1@gmail.com>
2023-03-21 10:33:02 +0700

    refactor: Delete MapData and store those contents into MapUtilites

--- Assets/_Scripts/Map/MapUtilities.cs
+++ Assets/_Scripts/Map/MapUtilities.cs
@@ -4,15 +4,18 @@ namespace Map
 {
     public static class MapUtilities
     {
+        public static readonly float ConversionConst = 0.000008950159495243074f;
+        public static readonly Vector2 GeoAnchor = new (10.772802f, 106.659716f);
+
         public static Vector2 GeoToWorldPosition(Vector2 geoPosition)
         {
-            var reversedPos = (geoPosition - MapData.GeoAnchor) / MapData.ConversionConst;
+            var reversedPos = (geoPosition - GeoAnchor) / ConversionConst;
             return new Vector2(reversedPos.y, reversedPos.x);
         }
         
         public static Vector2 WorldToGeoPosition(Vector2 geoPosition)
         {
-            var reversedPos = (geoPosition - MapData.GeoAnchor) / MapData.ConversionConst;
+            var reversedPos = (geoPosition - GeoAnchor) / ConversionConst;
             return new Vector2(reversedPos.y, reversedPos.x);
         }
     }

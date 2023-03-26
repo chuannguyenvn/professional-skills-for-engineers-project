eb73127fcdce675fc3fb945ab660606cbc115334
ShunPeng1 <shunpeng1@gmail.com>
2023-03-26 16:34:59 +0700

    feat: StateMachine

--- Assets/_Scripts/Manager/ApplicationManager.cs
+++ Assets/_Scripts/Manager/ApplicationManager.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ApplicationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            
+            StartCoroutine(nameof(Test));
+        }
+    }
+
+    IEnumerator Test()
+    {
+        Debug.Log("Hello world");
+        yield return new WaitForSeconds(10);
     }
 }
--- Assets/_Scripts/StateMachine/StateMachine.cs
+++ Assets/_Scripts/StateMachine/StateMachine.cs
@@ -1,14 +1,18 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Scripts.StateMachine
 {
-    public class StateMachine<TState> where TState : Enum  
+    public class StateMachine<TState> : MonoBehaviour where TState : Enum
     {
         private Dictionary<TState, State> _states = new ();
         private State _currentState;
 
+        private Queue<State> _changingStateQueue = new ();
+        private bool _isChangingState;
+        
         public void AddState(TState stateEnum, State state)
         {
             _states[stateEnum] = state;
@@ -21,15 +25,9 @@ namespace _Scripts.StateMachine
 
         public void SetState(TState stateEnum)
         {
-            if (_currentState != null)
-            {
-                _currentState.OnExitState();
-            }
-
             if (_states.TryGetValue(stateEnum, out State nextState))
             {
-                _currentState = nextState;
-                _currentState.OnEnterState();
+                _changingStateQueue.Enqueue(nextState);
             }
             else
             {
@@ -37,12 +35,24 @@ namespace _Scripts.StateMachine
             }
         }
 
-        public void Update()
+        private IEnumerator SwitchingState()
         {
-            if (_currentState != null)
+            if (_isChangingState)
             {
-            
+                yield break;
             }
+
+            _isChangingState = true;
+            while (_changingStateQueue.Count>0)
+            {
+                var nextState = _changingStateQueue.Dequeue();
+                yield return StartCoroutine(_currentState.OnExitState());
+                yield return StartCoroutine(nextState.OnEnterState());
+                _currentState = nextState;
+            }
+
+            _isChangingState = false;
         }
+        
     }
 }

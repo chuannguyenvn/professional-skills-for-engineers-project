2092088638b8465ff6837b6854e6a327a4a7eaaa
ShunPeng1 <shunpeng1@gmail.com>
2023-04-08 15:43:33 +0700

    test: State Machine switching but fail

--- Assets/_Scripts/Home/HomeStateMachine.cs
+++ Assets/_Scripts/Home/HomeStateMachine.cs
@@ -12,22 +12,31 @@ using UnityEngine.UI;
 public class HomeStateMachine : StateMachine<HomeStateMachine, AppState>
 {
     [SerializeField] private CanvasGroup _homeMenuCanvas;
-    [SerializeField] private TMP_InputField searchBar;
+    [SerializeField] private TMP_InputField _searchBar;
     [SerializeField] private Button _navigationButton;
     [SerializeField] private Button _calendarPageButton;
     
     private void Awake()
     {
+        InitEvent();
         AddToFunctionQueue(OnShow, StateEvent.OnEnter);
+        AddToFunctionQueue(OnHide, StateEvent.OnExit);
     }
 
+    private void InitEvent()
+    {
+        _navigationButton?.onClick.AddListener(()=> Debug.Log("Navigation in home"));
+        _calendarPageButton?.onClick.AddListener(() => ApplicationManager.Instance.SetState(AppState.Calendar));
+        _searchBar?.onSelect.AddListener((_) => ApplicationManager.Instance.SetState(AppState.Search));
+    }
+    
     void OnShow()
     {
         _homeMenuCanvas.alpha = 1;
         _homeMenuCanvas.interactable = true;
     }
 
-    private void OnDisable()
+    private void OnHide()
     {
         _homeMenuCanvas.alpha = 0;
         _homeMenuCanvas.interactable = false;

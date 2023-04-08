07fb5d07cbd9ff9d4ad68a817aedf16f2f6b7608
ShunPeng1 <shunpeng1@gmail.com>
2023-04-08 21:58:01 +0700

    refactor: create Calendar State

---
+++ Assets/_Scripts/State/CalendarStateMachine.cs
@@ -0,0 +1,10 @@
+using _Scripts.Manager;
+using _Scripts.StateMachine;
+
+namespace _Scripts.State
+{
+    public class CalendarStateMachine : StateMachine<CalendarStateMachine, AppState>
+    {
+        
+    }
+}

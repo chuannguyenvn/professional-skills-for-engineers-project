96dd1a90a72cf0ede725767f0f23687a3ff22bf0
ShunPeng1 <shunpeng1@gmail.com>
2023-04-18 11:53:34 +0700

    refactor: turn off road node visualize

--- Assets/_Scripts/Map/RoadIntersectionNode.cs
+++ Assets/_Scripts/Map/RoadIntersectionNode.cs
@@ -20,7 +20,7 @@ public class RoadIntersectionNode : MonoBehaviour
     }
 
     
-    
+    /*
     private void OnValidate()
     {
         if (adjacentRoadNodes == null) return;
@@ -31,5 +31,5 @@ public class RoadIntersectionNode : MonoBehaviour
             if(roadNode.adjacentRoadNodes.Find(node => node == this ) == null ) roadNode.adjacentRoadNodes.Add(this);
         }
     }
-    
+    */
 }
